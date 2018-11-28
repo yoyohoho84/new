93e71b1488bc6bfc4a4079a7f7b1eab3feb09f06
Kirill <yoyohoho84@gmail.com>
2018-11-28 21:49:04 +0300

    Updates

---
+++ precache-manifest.8ce9544af111f7c1ca5c86506bf3d501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a198f4cc6c13f64901b3",
+    "url": "/new/static/js/runtime~main.a198f4cc.js"
+  },
+  {
+    "revision": "56990f63faacf9885d1d",
+    "url": "/new/static/js/main.56990f63.chunk.js"
+  },
+  {
+    "revision": "c530ebe1d6d534a8822c",
+    "url": "/new/static/js/1.c530ebe1.chunk.js"
+  },
+  {
+    "revision": "56990f63faacf9885d1d",
+    "url": "/new/static/css/main.638e96c2.chunk.css"
+  },
+  {
+    "revision": "a331a14d3864e3374efe4770a98947e6",
+    "url": "/new/index.html"
+  }
+];
